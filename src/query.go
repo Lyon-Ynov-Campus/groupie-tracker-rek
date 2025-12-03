bca2b86680dd0633826216e5e161665ba9acfa8a
hdagbegn <dagbegnonedvige.houmasse@ynov.com>
2025-12-03 17:24:09 +0100

    separation du query insert a part dans un fichier

--- src/query.go
+++ src/query.go
@@ -1,8 +1,7 @@
 package server
 
 
-
-// Fonction pour inserer les données d'un nouvel utilisateur
+// Fonction pour inserer les données d'un nouvel utilisateur dans la base de données 
 
 func InsertValuesUser(pseudo, email, passwordHash string) error {
 	_, err := Rekdb.Exec("INSERT INTO users (pseudo, email, password_hash) VALUES (?, ?, ?)", pseudo, email, passwordHash)
